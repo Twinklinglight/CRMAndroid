7ffcb1651fc6dff74af2ef2b50e9e59ee0191117
shenzhongjia <shen123456>
2017-06-08 15:21:27 +0800

    申中佳标题头布局

--- app/src/main/java/com/wtcrmandroid/iat/Iat.java
+++ app/src/main/java/com/wtcrmandroid/iat/Iat.java
@@ -44,25 +44,21 @@ public class Iat {
 		this.context = context;
 		mIat = SpeechRecognizer.createRecognizer(context, mInitListener);// 使用SpeechRecognizer对象，可根据回调消息自定义界面；
 		mIatDialog = new RecognizerDialog(context, mInitListener);// 初始化听写Dialog，如果只使用有UI听写功能，无需创建SpeechRecognizer
-		mToast = Toast.makeText(context, "sdaf", Toast.LENGTH_SHORT);
+		mToast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
 		mIatResults = new LinkedHashMap<String, String>();
-		mToast.show();
-		if(mIat==null){
-			Toast.makeText(context, "空参数1", Toast.LENGTH_SHORT).show();
-		}
 	}
 
 	/**
 	 * 语音识别
 	 */
 	public void iatRecognize(){
-//		FlowerCollector.onEvent(context, "iat_recognize");// 移动数据分析，收集开始听写事件
-//		result="";// 清空显示内容
-//		mIatResults.clear();
+		FlowerCollector.onEvent(context, "iat_recognize");// 移动数据分析，收集开始听写事件
+		result="";// 清空显示内容
+		mIatResults.clear();
 		setParam();// 设置参数
 		mIatDialog.setListener(mRecognizerDialogListener);// 显示听写对话框
-//		mIatDialog.show();
-//		showTip("请开始说话…");
+		mIatDialog.show();
+		showTip("请开始说话…");
 	}
 
 	/**
@@ -81,9 +77,9 @@ public class Iat {
 	private InitListener mInitListener = new InitListener() {
 		@Override
 		public void onInit(int code) {
-			Log.e(TAG, "SpeechRecognizer init() code = " + code);
+			Log.d(TAG, "SpeechRecognizer init() code = " + code);
 			if (code != ErrorCode.SUCCESS) {
-				showTip("初始化失败，错误码：" + code);
+//				showTip("初始化失败，错误码：" + code);
 			}
 		}
 	};
@@ -179,20 +175,16 @@ public class Iat {
 	 * 参数设置
 	 */
 	private void setParam() {
-		if(mIat!=null) {
-			mIat.setParameter(SpeechConstant.PARAMS, null);// 清空参数
-			mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);// 设置听写引擎
-			mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");// 设置返回结果格式
-			mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");// 设置语言 简体中文
-			mIat.setParameter(SpeechConstant.ACCENT, "mandarin");//支持普通话
-			mIat.setParameter(SpeechConstant.VAD_BOS, "4000");// 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
-			mIat.setParameter(SpeechConstant.VAD_EOS, "1000");// 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
-			mIat.setParameter(SpeechConstant.ASR_PTT, "1");// 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
-			mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");// 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限  // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
-			mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/iat.wav");
-		}else {
-			Toast.makeText(context, "空参数", Toast.LENGTH_SHORT).show();
-		}
+		mIat.setParameter(SpeechConstant.PARAMS, null);// 清空参数
+		mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);// 设置听写引擎
+		mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");// 设置返回结果格式
+		mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");// 设置语言 简体中文
+		mIat.setParameter(SpeechConstant.ACCENT, "mandarin");//支持普通话
+		mIat.setParameter(SpeechConstant.VAD_BOS,"4000");// 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+		mIat.setParameter(SpeechConstant.VAD_EOS,"1000");// 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+		mIat.setParameter(SpeechConstant.ASR_PTT,"1");// 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+		mIat.setParameter(SpeechConstant.AUDIO_FORMAT,"wav");// 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限  // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
+		mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory()+"/msc/iat.wav");
 	}
 
 	/**
