e3ae819b39c7cc5e80abb17eb8621d2c79af17ba
shenzhongjia <shen123456>
2017-06-08 15:17:30 +0800

    申中佳标题头布局

---
+++ app/src/main/java/com/wtcrmandroid/model/WriterWeekPlaneData.java
@@ -0,0 +1,45 @@
+package com.wtcrmandroid.model;
+
+/**
+ * Created by 1363655717 on 2017-06-06.
+ * 写周计划实体类
+ */
+
+public class WriterWeekPlaneData {
+    private String tvPlan;
+    private String etWorkPlane;
+    private String etPlaneTarget;
+    private String etProportion;
+
+    public String getTvPlan() {
+        return tvPlan;
+    }
+
+    public void setTvPlan(String tvPlan) {
+        this.tvPlan = tvPlan;
+    }
+
+    public String getEtWorkPlane() {
+        return etWorkPlane;
+    }
+
+    public void setEtWorkPlane(String etWorkPlane) {
+        this.etWorkPlane = etWorkPlane;
+    }
+
+    public String getEtPlaneTarget() {
+        return etPlaneTarget;
+    }
+
+    public void setEtPlaneTarget(String etPlaneTarget) {
+        this.etPlaneTarget = etPlaneTarget;
+    }
+
+    public String getEtProportion() {
+        return etProportion;
+    }
+
+    public void setEtProportion(String etProportion) {
+        this.etProportion = etProportion;
+    }
+}
