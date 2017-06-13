a3d5709262ce8f703fe2be37b06e940f649c7c28
shenzhongjia <shen123456>
2017-06-13 09:15:14 +0800

    presenter提交

---
+++ app/src/main/java/com/wtcrmandroid/presenter/BasePresenter.java
@@ -0,0 +1,39 @@
+package com.wtcrmandroid.presenter;
+
+import com.wtcrmandroid.httpfactory.HttpRequest;
+import com.wtcrmandroid.httpfactory.callback.StringCallBack;
+import com.wtcrmandroid.utils.L;
+import com.wtcrmandroid.view.AllView;
+
+import java.util.Map;
+
+/**
+ * Created by 1363655717 on 2017-06-12.
+ */
+
+public abstract class BasePresenter<T> {
+    protected AllView<T> view;
+    public BasePresenter(AllView<T> view) {
+        this.view=view;
+    }
+    protected abstract void returnData(String response);
+    protected void post(String url, Map<String, String> params){
+        HttpRequest.instance().sendPost(url, params,null, new StringCallBack() {
+            @Override
+            public void onError(int errorRet, String errorMsg) {
+                L.e(errorMsg);
+            }
+
+            @Override
+            public void onResponse(String response) {
+                returnData(response);
+
+            }
+
+            @Override
+            public void onNetError(Exception e) {
+                L.e("出错了");
+            }
+        });
+    }
+}
