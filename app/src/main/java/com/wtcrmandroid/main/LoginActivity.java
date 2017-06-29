fab389d64c31a07aa142dc537dbc0b3a287f4865
wt-pc <teddy19930521@gmail.com>
2017-06-29 14:28:47 +0800

    6.29登录更新

--- app/src/main/java/com/wtcrmandroid/main/LoginActivity.java
+++ app/src/main/java/com/wtcrmandroid/main/LoginActivity.java
@@ -2,21 +2,34 @@ package com.wtcrmandroid.main;
 
 import android.content.Intent;
 import android.telephony.TelephonyManager;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
+import com.wtcrmandroid.BaseActivity;
 import com.wtcrmandroid.R;
 import com.wtcrmandroid.WTDataBaseManager;
-import com.wtcrmandroid.BaseActivity;
 import com.wtcrmandroid.model.reponsedata.LoginData;
 import com.wtcrmandroid.presenter.activity.LoginPresenter;
 import com.wtcrmandroid.utils.L;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 /**
  * Created by 1363655717 on 2017-06-12.
  */
 
-public class LoginActivity extends BaseActivity<LoginPresenter, LoginData> {
+public class LoginActivity extends BaseActivity<LoginPresenter,LoginData> {
+
+    @BindView(R.id.et_account)
+    EditText etAccount;
+    @BindView(R.id.et_pass)
+    EditText etPass;
+    @BindView(R.id.cb_passstyle)
+    CheckBox cbPassstyle;
 
     @Override
     protected int layout() {
@@ -25,10 +38,29 @@ public class LoginActivity extends BaseActivity<LoginPresenter, LoginData> {
 
     @Override
     protected void initView() {
-        presenter = new LoginPresenter(this);
+        presenter=new LoginPresenter(this);
         TelephonyManager tm = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);
-        L.e(tm.getDeviceId() + "--");
+        L.e(tm.getDeviceId()+"--");
         WTDataBaseManager.getsInstance().initDatabase(this);
+        etPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+        cbPassstyle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+                // TODO Auto-generated method stub
+                if(cbPassstyle.isChecked()){
+                    //设置EditText的密码为可见的
+                    etPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    etPass.setSelection(etPass.getText().toString().length());
+                }else{
+                    //设置密码为隐藏的
+                    etPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    etPass.setSelection(etPass.getText().toString().length());
+                }
+            }
+
+        });
 
     }
 
@@ -39,9 +71,9 @@ public class LoginActivity extends BaseActivity<LoginPresenter, LoginData> {
 
     @Override
     public void returnData(int key, LoginData data) {
-        L.e("返回数据" + key + data.toString());
+        L.e("返回数据"+key+data.toString());
         L.e(data.getUserName());
-        startActivity(new Intent(this, MainActivity.class));
+        startActivity(new Intent(this,MainActivity.class));
 
     }
 }
