a94af8b185f75fe2d538d6667d91885cf41113b6
wt-pc <teddy19930521@gmail.com>
2017-07-14 09:04:12 +0800

    7.14 动态权限

--- app/src/main/java/com/wtcrmandroid/BaseActivity.java
+++ app/src/main/java/com/wtcrmandroid/BaseActivity.java
@@ -1,10 +1,13 @@
 package com.wtcrmandroid;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.widget.Toast;
 
+import com.wtcrmandroid.permission.PermissionsActivity;
+import com.wtcrmandroid.permission.PermissionsChecker;
 import com.wtcrmandroid.presenter.BasePresenter;
 import com.wtcrmandroid.utils.L;
 import com.wtcrmandroid.view.AllView;
@@ -20,7 +23,16 @@ import butterknife.ButterKnife;
 
 public abstract class BaseActivity<T extends BasePresenter, T1> extends AppCompatActivity implements AllView<T1> {
     protected T presenter;
+    private static final int REQUEST_CODE = 0; // 请求码
+    private PermissionsChecker mPermissionsChecker; // 权限检测器
+    // 所需的全部权限
+    static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
 
+    };
     @Override
     public void showShortToast(String text) {
         L.e(text);
@@ -33,8 +45,8 @@ public abstract class BaseActivity<T extends BasePresenter, T1> extends AppCompa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(layout());
+        mPermissionsChecker = new PermissionsChecker(this);
         ButterKnife.bind(this);
         initView();
     }
@@ -50,5 +62,16 @@ public abstract class BaseActivity<T extends BasePresenter, T1> extends AppCompa
      * 初始化方法
      */
     protected abstract void initView();
+    @Override protected void onResume() {
+        super.onResume();
 
+        // 缺少权限时, 进入权限配置页面
+        if (mPermissionsChecker.lacksPermissions(PERMISSIONS)) {
+            startPermissionsActivity();
+        }
+    }
+
+    private void startPermissionsActivity() {
+        PermissionsActivity.startActivityForResult(this, REQUEST_CODE, PERMISSIONS);
+    }
 }
